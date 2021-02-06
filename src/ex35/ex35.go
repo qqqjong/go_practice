c9d3652213b01b7aa82bebfb97ea1b720a245774
qqqjong <qqqjong@gmail.com>
2021-02-06 21:42:31 +0900

    UTF-8

---
+++ src/ex35/ex35.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	friends := []string{"Annie", "Betty", "Charley", "Doug", "Edward"}
+	for _, v := range friends {
+		friends = friends[:2]
+		fmt.Printf("v[%s]\n", v)
+	}
+
+	friends = []string{"Annie", "Betty", "Charley", "Doug", "Edward"}
+	for i := range friends {
+		friends = friends[:2]
+		fmt.Printf("v[%s]\n", friends[i])
+	}
+}
